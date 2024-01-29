d0f1bb85ef22af343cf08e9be1a10be51122a422
Sarmigan Sritharan <sarmigan@live.co.uk>
2024-01-29 14:56:58 +0000

    added breakpoints

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -9,19 +9,19 @@ export default async function Page() {
     return (
         <div>
             <div className="flex flex-col justify-center items-center h-20">
-                <span id="music_typewriter" className="text-lg font-fira-code text-center"></span>
+                <span id="music_typewriter" className="text-lg xl:text-4xl font-fira-code text-center"></span>
                 <Script src="/typewriter.js" strategy="afterInteractive"></Script>
             </div>
             <div className="flex flex-col justify-center items-center">
                 {data.data.items.map((artist: any, index: any) => (
                     <div key={index} className="m-12 flex-col flex items-center justify-center hover:scale-110 duration-200">
-                        <Image 
+                        <img
                             key={index} 
-                            className="max-w-48 rounded shadow-2xl" 
+                            className="max-w-48 xl:max-w-72 rounded shadow-2xl" 
                             src={artist.images[0].url}
                             alt={artist.name}
                         />
-                        <p key={index} className="font-fira-code text-xs mt-2">{index+1}. {artist.name}</p>
+                        <p key={index} className="font-fira-code text-xs xl:text-lg mt-2">{index+1}. {artist.name}</p>
                     </div>
                 ))}
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Home(){
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <div className="flex">
-        <h1 id="welcome_typewriter" className="text-2xl font-bold font-fira-code"></h1><span className="border-b-2 m-1 w-3 h-6 border-black animate-caret"></span>
+        <h1 id="welcome_typewriter" className="text-2xl xl:text-6xl font-bold font-fira-code"></h1><span className="border-b-2 xl:border-b-4 mx-2 xl:mx-4 w-3 xl:w-9 h-7 xl:h-14 border-black animate-caret"></span>
         <Script src="/typewriter.js" strategy="afterInteractive"></Script>
       </div>
       <div className="flex mt-5">
-        <a className="font-fira-code text-xs mx-2 hover:scale-110 hover:underline underline-offset-3" href="https://www.linkedin.com/in/sarmigan/">LinkedIn</a>
-        <a className="font-fira-code text-xs mx-2 hover:scale-110 hover:underline underline-offset-3" href="https://github.com/Sarmigan">GitHub</a>
+        <a className="font-fira-code text-xs xl:text-xl mx-2 xl:mx-4 hover:scale-110 hover:underline underline-offset-3" href="https://www.linkedin.com/in/sarmigan/">LinkedIn</a>
+        <a className="font-fira-code text-xs xl:text-xl mx-2 xl:mx-4 hover:scale-110 hover:underline underline-offset-3" href="https://github.com/Sarmigan">GitHub</a>
       </div>
-      <a className="absolute bottom-0 right-0 m-2 font-fira-code text-lg font-bold" href="/music">+</a>
+      <a className="absolute bottom-0 right-0 m-2 xl:m-5 font-fira-code text-lg xl:text-4xl font-bold hover:scale-125" href="/music">+</a>
     </div>
   );
 }
