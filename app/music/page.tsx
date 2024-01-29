644136dde7ee673bd4e7aa68b3512209937b6239
Sarmigan Sritharan <sarmigan@live.co.uk>
2024-01-29 14:02:14 +0000

    add keys to iterators

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,4 +1,5 @@
 import Script from "next/script";
+import Image from "next/image";
 
 export default async function Page() {
     const res = await fetch(`http://localhost:3000/api/spotify/topartists`)
@@ -13,9 +14,14 @@ export default async function Page() {
             </div>
             <div className="flex flex-col justify-center items-center">
                 {data.data.items.map((artist: any, index: any) => (
-                    <div className="m-12 flex-col flex items-center justify-center hover:scale-110 duration-200">
-                        <img className="max-w-48 rounded shadow-2xl" src={artist.images[0].url}></img>
-                        <p className="font-fira-code text-xs mt-2">{index+1}. {artist.name}</p>
+                    <div key={index} className="m-12 flex-col flex items-center justify-center hover:scale-110 duration-200">
+                        <Image 
+                            key={index} 
+                            className="max-w-48 rounded shadow-2xl" 
+                            src={artist.images[0].url}
+                            alt={artist.name}
+                        />
+                        <p key={index} className="font-fira-code text-xs mt-2">{index+1}. {artist.name}</p>
                     </div>
                 ))}
             </div>
