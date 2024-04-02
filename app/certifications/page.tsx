a8f94e670fcbb8370d4d636eae01b4f5c9b84d93
Sarmigan Sritharan <sarmigan@live.co.uk>
2024-04-02 06:58:17 +0100

    change a to link on certifications

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import { Typewriter } from 'nextjs-simple-typewriter'
 
 export default function Page(){
   return (
     <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center h-16 xl:h-20">
-            <span className="text-2xl xl:text-5xl font-fira-code text-center">
+        <div className="flex flex-col justify-center items-center h-16 md:h-20">
+            <span className="text-2xl md:text-5xl font-fira-code text-center">
             <Typewriter
                 words={["certifications"]}
                 loop={1}
@@ -14,10 +15,10 @@ export default function Page(){
             />
             </span>
         </div>
-        <div className="my-6 w-[240px] xl:w-[720px]">
+        <div className="my-6 w-[240px] md:w-[720px]">
             <div className="mt-6">
-                <a href="https://www.credly.com/badges/3bea13b0-86ff-4980-986a-2407b357c50c/public_url" className="text-[11px] xl:text-lg font-fira-code hover:underline">aws certified solutions architecht - associate</a>
-                <p className="text-[11px] xl:text-lg font-fira-code">[issued march 2024 - expires march 2027]</p>
+                <Link target="_blank" href="https://www.credly.com/badges/3bea13b0-86ff-4980-986a-2407b357c50c/public_url" className="text-[11px] md:text-lg font-fira-code hover:underline">aws certified solutions architecht - associate</Link>
+                <p className="text-[11px] md:text-lg font-fira-code">[issued march 2024 - expires march 2027]</p>
             </div>
         </div>
     </div>
